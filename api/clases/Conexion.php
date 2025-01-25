4bf0724144aa9b53f5e8ea3751b9835b5e0b31fe
Juan Álvaro Flórez <jflorc01@estudiantes.unileon.es>
2025-01-25 22:26:29 +0100

    Update Conexion.php

--- api/clases/Conexion.php
+++ api/clases/Conexion.php
@@ -1,16 +1,35 @@
 <?php
-    Class Conexion extends mysqli {
-        private $host = "localhost";
-        private $db = "tapleon";
-        private $user = "tapleon";
-        private $pass = "tapleon";
-        public function __construct() {
-            try {
-                parent::__construct($this->host, $this->user, $this->pass, $this->db);
-            } catch (mysqli_sql_exception $e) {
-                //echo "ERROR: {$e->getMessage()}";
-                header("HTTP/1.1 500 Internal Server Error");
-                exit;
-            }
+/**
+ * Clase para gestionar la conexión a la base de datos.
+ * 
+ * Extiende la clase mysqli para proporcionar una conexión a la base de datos.
+ * 
+ * PHP version 8.2.12
+ * 
+ * @category Database
+ * @package  ConcursoTapas
+ * @author   Isaac Alonso
+ */
+
+class Conexion extends mysqli {
+    private $host = "localhost";
+    private $db = "tapleon";
+    private $user = "tapleon";
+    private $pass = "tapleon";
+
+    /**
+     * Constructor de la clase Conexion.
+     * 
+     * Inicializa la conexión a la base de datos utilizando los parámetros definidos.
+     * 
+     * @throws mysqli_sql_exception Si hay un error al conectar a la base de datos.
+     */
+    public function __construct() {
+        try {
+            parent::__construct($this->host, $this->user, $this->pass, $this->db);
+        } catch (mysqli_sql_exception $e) {
+            header("HTTP/1.1 500 Internal Server Error");
+            exit;
         }
-    } 
+    }
+}
